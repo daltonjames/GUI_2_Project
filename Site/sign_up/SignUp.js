f196b7e5a36bd4108d7e46bfad575f1a6e559340
Dalton James <dalton_james@student.uml.edu>
2015-03-08 12:55:50 -0400

    Dalton alpha push

--- Site/sign_up/SignUp.js
+++ Site/sign_up/SignUp.js
@@ -9,52 +9,72 @@
 //
 
 var data;
+var success = true;
 
 $(document).ready(function() {
 
 	$("#SignUp_form").submit(function(){
 
+		//error checking stuff
+		$(".error").empty();
+		success = true;
+	
+/*	
 		console.log( "FirstName = " + $("#FirstName").val() );
 		console.log( "LastName = " + $("#LastName").val() );
 		console.log( "Username = " + $("#Username").val() );
 		console.log( "Email = " + $("#Email").val() );
 		console.log( "Password = " + $("#Password").val() );
 		console.log( "c_Password = " + $("#c_Password").val() );
-
+*/
 
 		if($("#Password").val() !== $("#c_Password").val() )
+		{
 			console.log("Passwords do not match");
+			success = false;
+			$(".error").append("Passwords do not match");
+		}
 		else
+		{
 			console.log("The PW matches");
+		}
 		
 
+		//serialize data to pass to php 
 		var $this = $(this);
 		formData = $this.serialize();
 
 		console.log("");
 		console.log(formData);
 
-		$.ajax({
-			type: 'POST',
-			url: 'SignUp.php',
-			data: formData,
-			success: function(data){
-				console.log("data = " + data);
-				var url = window.location.href;
-				url = url.replace("sign_up", "home");
-				window.location = url;
-			},
-		});
-		
-//		$.post( "SignUp.php", formData, function(data){
-//
-//			console.log(data);
-//		
-//		});
-
-
-
+		if(success == true)
+		{
+			$.ajax({
+				type: 'POST',
+				url: 'SignUp.php',
+				data: formData,
+				success: function(data){
+					console.log("data = " + data);
+					var p_data = jQuery.parseJSON(data);
+					console.log("p_data = " + p_data);
+					if( p_data.status == 'error' )
+					{
+						console.log("message = " + p_data.message);
+						$(".error").append("Username already exists");
+					}
+					else
+					{
+						$.get( "page1.php", formData ).done(function( data ) {
+							console.log("data from page1 = " + data );
+						});
+						
+						
+						var url = window.location.href;
+						url = url.replace("sign_up", "home");
+						window.location = url;
+					}
+				},	
+			});
+		}
 	});
-
-
 });
--- Site/sign_up/SignUp.php
+++ Site/sign_up/SignUp.php
@@ -9,26 +9,35 @@ if ( $database->connect_errno > 0 ) {
 }
 
 $check_username = $_POST[Username];
-echo $check_username;
-echo "\n";
+#echo $check_username;
+#echo "\n";
 	
 $sql = 'SELECT username FROM alpha_acct WHERE username="' . $check_username . '"';
 
 if ( ! $result = $database->query( $sql ) ) {
-   die( '<p>Error retreving data [' . $database->error . ']</p>' );
+   die( '<p>Error retrieving data [' . $database->error . ']</p>' );
 }
 
+$response = array();
+
 if(mysqli_num_rows($result)>=1){
-    echo "name already exists";
+	$response['status'] = 'error';
+	$response['message'] = 'username already exists';
 }
 else	
 {
-	echo "name being added";
+	
 	$sql = "INSERT INTO alpha_acct (`index`, `username`, `password`, `email`) VALUES ( null, '$_POST[Username]' , '$_POST[Password]' , '$_POST[Email]' )"; 	
 
 	if ( ! $result = $database->query( $sql ) ) {
-		die( '<p>Error retreving data [' . $database->error . ']</p>' );
+		die( '<p>Error retrieving data [' . $database->error . ']</p>' );
 	}
+	
+	$response['status'] = 'success';
+	$response['message'] = 'acc created';
+
 }
+	
+	echo json_encode($response);
 	
 ?>
