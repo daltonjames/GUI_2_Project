4fc2dcc69ea28e31cecec3bf7d6aeb592b8d1a84
Pkthunder <jared_perreault@yahoo.com>
2015-03-24 00:30:16 -0400

    Added jQuery Functionality to Angular
    And fixed the corresponding bugs this created

--- Site/home/app.js
+++ Site/home/app.js
@@ -17,23 +17,21 @@ App.controller("AppCtrl", function($scope,$http) {
 			$scope.error = error;
 		});
 
-	//TODO: Fix all functionality below to work with jQuery (currently will break). Investigation how to link jQuery first and use it with AngularJS.
-
 	//On click event to handle navigation to each individual topic's view topics page
 	$scope.navigate = function($event) {
 		var ele = angular.element($event.target);
 		
-		//if click is on See More anchor
+		//if click is on See More anchor tag
 		if ( ele.hasClass('see-more') ) {
-			var descDiv = angular.element(ele.parent().parent().children()[1]); //jqLite functions are not very complete compared to jQuery
+			var descDiv = angular.element($event.currentTarget).children('div.topic-desc'); //targets topic-desc div (uses jQuery functionality)
 			//toggle the height of the description
-			if ( descDiv.prop('clientHeight') > 140 ) { //if the current state is NOT minimized
-				descDiv.css('height', 140); //sets height to 140 aka minimized
+			if ( descDiv.height() > 140 ) { //if the current state is NOT minimized
+				descDiv.height(140); //sets height to 140 aka minimized
 				ele.text('See More'); //changes anchor tag text
 			}
 			else { //if the current state IS minimized
 				descDiv.css('max-height', 'none'); //eliminates the max height set by css
-				descDiv.css('height', descDiv.prop('scrollHeight')); //sets height to scrollHeight aka elements actual height
+				descDiv.height(descDiv.prop('scrollHeight')); //sets height to scrollHeight aka elements actual height
 				ele.text('See Less'); //changes anchor tag text
 			}
 		}
@@ -49,16 +47,19 @@ App.controller("AppCtrl", function($scope,$http) {
 
 });
 //Check if each ng-repeat element overflow's (greater than max-height) and adds 'See More' functionality
-App.directive('seeMoreDirective', function() {
+App.directive('seeMoreDirective', function( $timeout ) {
 	return function(scope, element) {
-		var ele = angular.element(element);
-		ele.ready(function() {
+		var ele = angular.element(element); //wrapped jQuery object (done by angular)
+		
+		//$timeout waits until the browser has finished rendering the DOM (aka the ng-repeat elements)
+		$timeout( function() {
+			//checks for overflow within the #topic-desc div
 			if ( ele.prop('offsetHeight') < ele.prop('scrollHeight') ) {
 				//overflow detected
 				console.log('overflow detected');
-				ele.addClass('see-more');
-				var seeMore = angular.element("<p class='see-more'><a class='see-more'>See More</a></p>");
-				element.after(seeMore);
+				ele.addClass('see-more'); //adds overflow: hidden to css
+				var seeMore = angular.element("<p class='see-more'><a class='see-more'>See More</a></p>"); //adds See More/See Less button
+				element.after(seeMore); //inserts into the DOM right after #topic-desc div (and before the Tag List)
 			}
 		});
 	};
